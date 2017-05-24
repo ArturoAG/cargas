a933494d79dbb17047b67dbeb17133782cb02aae
ArturoAG <noe_170695@hotmail.com>
2017-05-24 14:31:00 -0500

    Update script pydio.sql

--- script pydio.sql
+++ script pydio.sql
@@ -1,5 +1,5 @@
-CREATE USER 'pydio'@'%' IDENTIFIED BY 'GWN6phWMeHW2VTnQ';
-GRANT ALL PRIVILEGES ON * . * TO 'pydio'@'%';
+CREATE USER 'pydio'@'localhost' IDENTIFIED BY 'GWN6phWMeHW2VTnQ';
+GRANT ALL PRIVILEGES ON * . * TO 'pydio'@'localhost';
 FLUSH PRIVILEGES;
 
 
